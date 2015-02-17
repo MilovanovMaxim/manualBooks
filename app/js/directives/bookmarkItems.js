ab2612a245126d4689ecdb9256668711d70037c0
Robert <robert@ucluster.ru>
2015-02-17 19:13:33 +0500

    переход по закладке

--- app/js/directives/bookmarkItems.js
+++ app/js/directives/bookmarkItems.js
@@ -4,28 +4,42 @@
             restrict: 'E',
             templateUrl: 'tpl/blocks/bookmarkItems.html',
             scope: {},
-            controller: ["$scope", "apiService", function ($scope, apiService) {
+            controller: ["$scope", "apiService", "$state", function ($scope, apiService, $state) {
 
                 $scope.displayBookmarks = function () {
                     return apiService.books.displayBookmarks().then(function (data) {
                         $scope.bookmarks = [];
-                        _.each(data.items, function (item) {
+                        if (data.items.length == 0) {
                             $scope.bookmarks.push(
                                 {
-                                    short_title: item.short_title,
-                                    full_title: item.full_title,
-                                    subtitle: item.subtitle,
-                                    media_image: item.media_image,
-                                    page_id: item.page_id
-                                }
-                            );
-                        });
+                                    short_title: 'No pages have been pinned',
+                                    full_title: 'No pages have been pinned',
+                                    media_image: ''
+                                });
+                        }
+                        else {
+                            _.each(data.items, function (item) {
+                                $scope.bookmarks.push(
+                                    {
+                                        short_title: item.short_title,
+                                        full_title: item.full_title,
+                                        subtitle: item.subtitle,
+                                        media_image: item.media_image,
+                                        page_id: item.page_id,
+                                        manual_id: item.manual_id,
+                                        manual_version: item.manual_version,
+                                        manual_title: item.manual_title
+                                    }
+                                );
+                            });
+                        }
                     });
                 };
 
-                $scope.openPage= function(pageId){
-
+                $scope.goToPage = function (manualId, title, manualVersion, pageId) {
+                    $state.go('show.guidepages', {fold: manualId, title: title, version: manualVersion, page: pageId});
                 };
+
             }]
         }
     })
