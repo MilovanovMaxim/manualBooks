a9352eee695bb8f5267179a8bfea689992325f69
MilovanovMaxim <denis@denis.ru>
2015-02-18 22:51:35 +0300

    fix recents

--- app/js/directives/recentActivity.js
+++ app/js/directives/recentActivity.js
@@ -4,12 +4,28 @@ angular.module('app').directive('mbRecentActivity', function(){
         templateUrl:'tpl/blocks/recentActivity.html',
         scope:true,
         replace:true,
-        controller: ["$scope", "apiService", function($scope, apiService){
+        controller: ["$scope", "apiService",'$state', function($scope, apiService, $state){
             apiService.account
                 .getRecentActivity()
                 .then(function(data){
                     $scope.recentActivity = data.items;
                 });
+
+            var openRules = {
+                page: function(recent){
+                    $state.go('show.page', {fold:recent.id});
+                },
+                manual: function(recent){
+                    $state.go('show.page', {fold:recent.id});
+                }
+            };
+
+            $scope.open = function(recent){
+                if (openRules[recent.type]) {
+                    openRules[recent.type](recent)
+                }
+            };
+
         }]
     }
 })
