670b2359edf5aa16f7960a8b3feced13ffe0c29b
Robert <robert.sabiryanov@gmail.com>
2015-02-23 22:22:53 +0500

    fix #21

--- app/js/services/profileService.js
+++ app/js/services/profileService.js
@@ -2,30 +2,31 @@
     module.factory('profileService', ['localStorageService', function (localStorageService) {
         var key = 'account';
         var service = {};
-        var profile = {};
+       
         service.isAdmin = function () {
+            var profile = service.getProfile();
             if (profile)
                 return profile.type == 'admin';
             return false;
         };
         service.isSuperAdmin = function () {
+            var profile = service.getProfile();
             if (profile)
                 return profile.type == 'superadmin';
             return false;
         };
         service.saveProfile = function (data) {
-            profile = data;
             localStorageService.setItem(key, JSON.stringify(data));
         };
         service.getProfile = function () {
-            profile = JSON.parse(localStorageService.getItem(key));
+            var profile = JSON.parse(localStorageService.getItem(key));
             return profile;
         };
         service.clearProfile = function () {
-            profile = {};
             localStorageService.removeItem(key);
         };
         service.getUserId = function () {
+            var profile = service.getProfile();
             if (profile && profile.id)
                 return profile.id;
             return 0;
