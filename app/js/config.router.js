26de9a3e2a7dbd27be92cefde9ffdb3d813a3154
MilovanovMaxim <denis@denis.ru>
2015-02-25 11:56:18 +0300

    fix login

--- app/js/config.router.js
+++ app/js/config.router.js
@@ -26,7 +26,7 @@ angular.module('app')
                       hasRole = false;
 
                   _.each(roles, function(role){
-                      hasRole = hasRole || profileService.hasRole(role);
+                      hasRole = hasRole | profileService.hasRole(role);
                   });
 
                   if (!hasRole){
