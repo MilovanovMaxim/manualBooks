f7bf5a7728782c2d48b40bdbed40ee8879b323a1
srobertz <robert.sabiryanov@gmail.com>
2015-04-01 22:53:33 +0500

    Edit buttons added to guideversions page

--- app/js/controllers/guideversions.js
+++ app/js/controllers/guideversions.js
@@ -1,21 +1,25 @@
-angular.module('app').controller('GuideversionsControlle', ['$scope', "apiService",'$stateParams', 'usSpinnerService', function($scope, apiService, $stateParams, usSpinnerService){
+angular.module('app').controller('GuideversionsControlle', ['$scope', "apiService", '$stateParams', 'usSpinnerService', 'profileService',
+    function ($scope, apiService, $stateParams, usSpinnerService, profileService) {
 
-    $scope.currentPage = 0;
-    $scope.pagSize = 15;
+        $scope.currentPage = 0;
+        $scope.pagSize = 15;
 
-    usSpinnerService.spin('mainSpiner');
+        usSpinnerService.spin('mainSpiner');
 
-    $scope.pageChanged = function(){
-    };
+        $scope.pageChanged = function () {
+        };
+        $scope.isAdmin = function () {
+            return profileService.isAdmin() || profileService.isSuperAdmin();
+        }
 
-    apiService.books
-        .getVersions($stateParams.fold)
-        .then(function(data){
-            usSpinnerService.stop('mainSpiner');
+        apiService.books
+            .getVersions($stateParams.fold)
+            .then(function (data) {
+                usSpinnerService.stop('mainSpiner');
 
-            $scope.manual = data.items[0];
-            $scope.bookVersions = data.items;
-            return data.items
-        });
+                $scope.manual = data.items[0];
+                $scope.bookVersions = data.items;
+                return data.items
+            });
 
-}]);
+    }]);
